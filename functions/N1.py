2ce67ccc289848c68807c38db0efdcc00dc2b47a
Kevin Kwan <kevin.7070@gmail.com>
2021-09-09 17:20:59 +0800

    start N1 fakes

---
+++ functions/N1.py
@@ -0,0 +1,135 @@
+import random
+from PIL import ImageFont
+
+def time_(text, draw):
+    x, y = 1041, 76
+    y -= 8
+    color = (229, 229, 229)
+    font = ImageFont.truetype('font/Roboto/Roboto-Regular.ttf', 38)
+
+    bg_color = (0, 0, 0)
+    w, h = font.getsize(text)
+    draw.rectangle((x, y, x + w, y + h), fill=bg_color)
+
+    draw.text(
+        (x, y), text, color, font=font
+    )
+
+
+def n1pci_(text, la, lo, draw):
+    x, y = 304, 1726
+    y -= 8
+    color = (117, 117, 117)
+    font = ImageFont.truetype('font/Roboto/Roboto-Regular.ttf', 34)
+
+    bg_color = (255, 255, 255)
+    w, h = font.getsize(text)
+    draw.rectangle((x, y, x + w, y + h), fill=bg_color)
+
+    draw.text(
+        (x, y), text, color, font=font
+    )
+
+    # la
+    text = la
+    x, y = 963, 932
+    y -= 8
+    color = (117, 117, 117)
+    font = ImageFont.truetype('font/Roboto/Roboto-Regular.ttf', 34)
+
+    bg_color = (255, 255, 255)
+    w, h = font.getsize(text)
+    draw.rectangle((x, y, x + w, y + h), fill=bg_color)
+
+    draw.text(
+        (x, y), text, color, font=font
+    )
+
+    # lo
+    text = lo
+    x, y = 963, 1034
+    y -= 8
+    color = (117, 117, 117)
+    font = ImageFont.truetype('font/Roboto/Roboto-Regular.ttf', 34)
+
+    bg_color = (255, 255, 255)
+    w, h = font.getsize(text)
+    draw.rectangle((x, y, x + w, y + h), fill=bg_color)
+
+    draw.text(
+        (x, y), text, color, font=font
+    )
+
+def n1ltepci_(text, la, lo, draw):
+    x, y = 303, 1651
+    y -= 8
+    color = (117, 117, 117)
+    font = ImageFont.truetype('font/Roboto/Roboto-Regular.ttf', 34)
+
+    bg_color = (255, 255, 255)
+    w, h = font.getsize(text)
+    draw.rectangle((x, y, x + w, y + h), fill=bg_color)
+
+    draw.text(
+        (x, y), text, color, font=font
+    )
+
+    # la
+    text = la
+    x, y = 963, 932
+    y -= 8
+    color = (117, 117, 117)
+    font = ImageFont.truetype('font/Roboto/Roboto-Regular.ttf', 34)
+
+    bg_color = (255, 255, 255)
+    w, h = font.getsize(text)
+    draw.rectangle((x, y, x + w, y + h), fill=bg_color)
+
+    draw.text(
+        (x, y), text, color, font=font
+    )
+
+    # lo
+    text = lo
+    x, y = 963, 1034
+    y -= 8
+    color = (117, 117, 117)
+    font = ImageFont.truetype('font/Roboto/Roboto-Regular.ttf', 34)
+
+    bg_color = (255, 255, 255)
+    w, h = font.getsize(text)
+    draw.rectangle((x, y, x + w, y + h), fill=bg_color)
+
+    draw.text(
+        (x, y), text, color, font=font
+    )
+
+
+def n1speed_(draw):
+    # downlink
+    text = "144"
+    x, y = 382, 313
+    color = (255, 255, 255)
+    font = ImageFont.truetype('font/Gauge-Mono-Regular.ttf', 134)
+
+    bg_color = (26, 27, 47)
+    w, h = font.getsize(text)
+    draw.rectangle((x, y, x + w, y + h), fill=bg_color)
+
+    draw.text(
+        (x, y), text, color, font=font
+    )
+
+    # uplink
+    text = "79.2"
+    x, y = 691, 314
+    color = (255, 255, 255)
+    font = ImageFont.truetype('font/Gauge-Mono-Regular.ttf', 134)
+
+    bg_color = (26, 27, 47)
+    w, h = font.getsize(text)
+    draw.rectangle((x, y, x + w, y + h), fill=bg_color)
+
+    draw.text(
+        (x, y), text, color, font=font
+    )
